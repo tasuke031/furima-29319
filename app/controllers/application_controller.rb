58382af6b7697ed709a28eac116533781fb3b9f6
tasuke031 <yosuket031@gmail.com>
2020-09-01 15:29:32 +0900

    バリデーションの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters
+  
+  def new
+    @user = User.new
+  end
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_reading, :first_reading, :birthday])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname, format: { with: /\A[a-z0-9]+\z/i, message: "nicknameを入力して下さい" }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i  }
+    #passwordは半角英数混合 /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください" }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください" }
+    validates :family_reading, format: { with: /\A[ァ-ン]/, message: "全角で入力してください" }
+    validates :first_reading, format: { with: /\A[ァ-ン]/, message: "全角で入力してください" }
+    
+    validates :birthday
+  end         
 end
