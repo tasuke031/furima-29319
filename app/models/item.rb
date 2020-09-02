e99f84ca81869f9b0a82710ce0b5fa71f3fb2e9a
tasuke031 <yosuket031@gmail.com>
2020-09-02 16:40:20 +0900

    Itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,5 +10,16 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :price, format: { with: /\A\d/, message: 'half-width number'}
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'out of setting range' }
+    validates :category, numericality: { other_than: 0, messege: 'select' }
+    validates :status, numericality: { other_than: 0, messege: 'select' }
+    validates :delivery_fee, numericality: { other_than: 0, messege: 'select' }
+    validates :delivery_day, numericality: { other_than: 0, messege: 'select' }
+    validates :prefecture, numericality: { other_than: 0, messege: 'select' }
+  end
 end
