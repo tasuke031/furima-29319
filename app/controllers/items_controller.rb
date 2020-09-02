194b60d1f8ed4f9194975f51e499cb69a8fffac0
tasuke031 <yosuket031@gmail.com>
2020-09-02 18:26:37 +0900

    itemsテーブルにデータが保存されることを確認

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,6 +16,7 @@ class ItemsController < ApplicationController
       redirect_to root_path
     else
       render :new
+    end
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,4 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
-  belongs_to_active_hash :status
-  belongs_to_active_hash :delivery_fee
-  belongs_to_active_hash :delivery_day
-  belongs_to_active_hash :prefecture
-  
 
   belongs_to :user
   has_one_attached :image
