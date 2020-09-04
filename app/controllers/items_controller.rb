21ac8d181e88b89182a9cb77c806b2e56f6673d3
tasuke031 <yosuket031@gmail.com>
2020-09-04 10:30:49 +0900

    商品詳細ページの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.new
     @items = Item.includes(:user).order('created_at DESC')
   end
 
@@ -17,6 +18,10 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
