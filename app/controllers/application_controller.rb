1302c3e2ca3fdc5a999dee2c731c65fc665484ae
tasuke031 <yosuket031@gmail.com>
2020-09-08 19:58:48 +0900

    Basic認証の有効化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  #before_action :basic_auth
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
@@ -9,9 +9,9 @@ class ApplicationController < ActionController::Base
       [:nickname, :family_name, :first_name, :family_reading, :first_reading, :birthday])
   end
 
-  #def basic_auth
-  #  authenticate_or_request_with_http_basic do |username, password|
-  #    username == ENV["BASIC_AUTH_USER"] && password ==ENV["BASIC_AUTH_PASSWORD"]
-  #  end
-  #end
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password ==ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
